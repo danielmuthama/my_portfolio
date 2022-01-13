c4fd4e638271d65897f20d5b4924d131263c2e10
Daniel Muthama <40577986+danielmuthama@users.noreply.github.com>
2022-01-13 06:15:49 +0300

    upload files

---
+++ script.js
@@ -0,0 +1,92 @@
+function feedBack(){
+    let myName = document.getElementById("name").value;
+    let myEmail = document.getElementById("email").value;
+    let mySubject = document.getElementById("subject").value;
+
+    if( myName == null || myName == ""){
+        alert("Please enter your Name");
+        return false;
+    }else if(myEmail == null || myEmail == ""){
+        alert("Please enter a valid email address");
+        return false;
+    }else if(mySubject == null || mySubject == ""){
+         alert("Please enter a valid subject");
+        return false;
+    }else{
+        alert("Welcome " + myName + " your message has been received and we will respond ASAP");
+    }
+}
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Software-Developer", "Network-Eng.", "Designer", "Freelancer", "Telecommunication-Eng."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Software-Developer", "Network-Eng.", "Designer", "Freelancer", "Telecommunication-Eng."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
