c498b9bdffffcd54b9901713c9109d2f00dd4cb8
Daniel Muthama <40577986+danielmuthama@users.noreply.github.com>
2022-03-15 02:16:37 +0300

    update files

--- script.js
+++ script.js
@@ -1,82 +1,82 @@
-function feedBack(){
+function feedBack() {
     let myName = document.getElementById("name").value;
     let myEmail = document.getElementById("email").value;
     let mySubject = document.getElementById("subject").value;
 
-    if( myName == null || myName == ""){
+    if (myName == null || myName == "") {
         alert("Please enter your Name");
         return false;
-    }else if(myEmail == null || myEmail == ""){
+    } else if (myEmail == null || myEmail == "") {
         alert("Please enter a valid email address");
         return false;
-    }else if(mySubject == null || mySubject == ""){
-         alert("Please enter a valid subject");
+    } else if (mySubject == null || mySubject == "") {
+        alert("Please enter a valid subject");
         return false;
-    }else{
+    } else {
         alert("Welcome " + myName + " your message has been received and we will respond ASAP");
     }
 }
 
 function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read More"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read Less"; 
-    moreText.style.display = "inline";
-  }
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read Less";
+        moreText.style.display = "inline";
+    }
 }
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function() {
+    $(window).scroll(function() {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function() {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Freelancer", "Software-Developer", "Data Analyst", "Network Engineer", "UI|UX Designer", "Telecom. Engineer"],
+        strings: ["Freelancer", "Software Developer", "Data Analyst", "Network Engineer", "UI|UX Designer", "Telecom. Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Freelancer", "Software-Developer", "Data Analyst", "Network Engineer", "UI|UX Designer", "Telecom. Engineer"],
+        strings: ["Freelancer", "Software Developer", "Data Analyst", "Network Engineer", "UI|UX Designer", "Telecom. Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -90,15 +90,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
